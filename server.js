a08ca7aef1084d31bc9ab324b48480784801ea80
nvdinh185 <nvdinh0766777123@gmail.com>
2023-06-09 09:22:55 +0700

    rv

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 ﻿const express = require('express');
 const app = express();
-const path = require('path');
 const errorHandler = require('./_helpers/error-handler');
 
-const publicPath = path.join(__dirname, 'client');
-app.use(express.static(publicPath));
+app.use(express.static(__dirname + '/client'));
 
 // api routes
 app.use('/hoa', require('./hoa/hoa.controller'));
 
 // global error handler
 app.use(errorHandler);
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
 
 // start server
 const port = process.env.PORT || 3000;
