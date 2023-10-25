4485eee1d1b8750e5b2a7ce5aae10c151661dcdf
nvdinh185 <nvdinh0766777123@gmail.com>
2023-10-25 16:08:27 +0700

    x

--- js/main.js
+++ js/main.js
@@ -7,19 +7,19 @@ const ulElement = $("#list");
 const listNews = [
     {
         id: 1,
-        img: 'images/h1.jpg',
+        img: 'h1.jpg',
         title: 'Cách phối đồ đi hẹn hò 8/3 đẹp như Quỳnh Anh Shyn',
         content: '8/3 sắp đến rồi, những gợi ý mặc đẹp dưới đây sẽ giúp bạn chọn được bộ đồ ưng ý. Cùng học Quỳnh Anh Shyn một vài cách phối đồ cực hay ho để bạn đi hẹn hò 8/3 nhé!'
     },
     {
         id: 2,
-        img: 'images/h2.jpg',
+        img: 'h2.jpg',
         title: 'Diện đồ đôi cho teen đi hẹn hò ngày 8/3',
         content: 'Những cặp đôi yêu nhau đã có ý tưởng nên mặc gì trong ngày 8/3 chưa? Những bộ đồ đôi vừa đẹp vừa thoải mái chắc chắn sẽ là gợi ý ăn mặc cho các bạn trẻ. Dưới đây là những bộ đồ đôi đẹp để teen tham ...'
     },
     {
         id: 3,
-        img: 'images/h3.jpg',
+        img: 'h3.jpg',
         title: 'Hướng dẫn chọn trang phục cho teengirl ngày 8/3',
         content: 'Các teen girl nhà mình hẳn đang rất hào hứng chọn những bộ đồ thật đẹp để đi chơi 8/3. Những gợi ý ăn mặc dưới đây sẽ giúp các nàng có những set đồ thật đẹp để diện trong ngày 8.3 nhé!'
     }
@@ -29,7 +29,7 @@ const listNews = [
 // listNews.forEach(function (news) {
 //     const liElement = $('<li>');
 //     liElement.html(`
-//         <a href="http://google.com/"><img src=${news.img} alt=${news.img} /></a>
+//         <a href="http://google.com/"><img src=images/${news.img} alt=${news.img} /></a>
 //         <div class="khoiphai">
 //             <h2><a href="http://google.com/">${news.title}</a></h2>
 //             <p>${news.content}</p>
@@ -46,7 +46,7 @@ const listNews = [
 // listNews.forEach(function (news) {
 //     htmls += `
 //         <li>
-//             <a href="http://google.com/"><img src=${news.img} alt=${news.img} /></a>
+//             <a href="http://google.com/"><img src=images/${news.img} alt=${news.img} /></a>
 //             <div class="khoiphai">
 //                 <h2><a href="http://google.com/">${news.title}</a></h2>
 //                 <p>${news.content}</p>
@@ -61,7 +61,7 @@ const listNews = [
 var htmls = listNews.map(function (news) {
     return `
         <li>
-            <a href="http://google.com/"><img src=${news.img} alt=${news.img} /></a>
+            <a href="http://google.com/"><img src=images/${news.img} alt=${news.img} /></a>
             <div class="khoiphai">
                 <h2><a href="http://google.com/">${news.title}</a></h2>
                 <p>${news.content}</p>
